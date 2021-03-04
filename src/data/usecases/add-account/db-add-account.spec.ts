959bd17658c1fd9e8801e108ac94d92c1d6f1d84
felipedias-dev <felipedias.dev@gmail.com>
2021-03-04 20:11:12 -0300

    refactor: update folder names

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -8,8 +8,8 @@ interface SutTypes {
 
 const makeEncrypter = (): Encrypter => {
   class EncrypterStub implements Encrypter {
-    async encrypt (value: string): Promise<string> {
-      return await new Promise(resolve => resolve('hashed_password'))
+    async encrypt(value: string): Promise<string> {
+      return await new Promise((resolve) => resolve('hashed_password'))
     }
   }
   return new EncrypterStub()
@@ -39,7 +39,11 @@ describe('DbAccount Usecase', () => {
 
   test('Should throw if Encripter throws', async () => {
     const { sut, encrypterStub } = makeSut()
-    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    jest
+      .spyOn(encrypterStub, 'encrypt')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      )
     const accountData = {
       name: 'valid_name',
       email: 'valid_email',
